d3a0f0010888b5d8ea9178b08c1b25689d7b6235
Michal Kacprzak <michal.kacprzak999@gmail.com>
2021-02-14 11:40:10 +0100

    react integration using webpack and babel | first component

--- music_controller/music_controller/urls.py
+++ music_controller/music_controller/urls.py
@@ -19,6 +19,7 @@ from django.urls.conf import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('api.urls'))
+    path('api/', include('api.urls')), 
+    path('', include('frontend.urls'))
 ]
 
