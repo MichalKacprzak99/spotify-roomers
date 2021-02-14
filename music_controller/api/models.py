c50d9b36f7bb8e044306ec02ca77c3c0942a9c47
Michal Kacprzak <michal.kacprzak999@gmail.com>
2021-02-14 13:36:02 +0100

    update Room model

--- music_controller/api/models.py
+++ music_controller/api/models.py
@@ -1,6 +1,8 @@
+  
 from django.db import models
 import string
 import random
+
 
 def generate_unique_code():
     length = 6
@@ -9,13 +11,14 @@ def generate_unique_code():
         code = ''.join(random.choices(string.ascii_uppercase, k=length))
         if Room.objects.filter(code=code).count() == 0:
             break
-    
+
     return code
 
-# Create your models here.
+
 class Room(models.Model):
-    code = models.CharField(max_length=8, default="", unique=True)
+    code = models.CharField(
+        max_length=8, default=generate_unique_code, unique=True)
     host = models.CharField(max_length=50, unique=True)
-    quest_can_pose = models.BooleanField(null=False, default=False)
+    guest_can_pause = models.BooleanField(null=False, default=False)
     votes_to_skip = models.IntegerField(null=False, default=1)
     created_at = models.DateTimeField(auto_now_add=True)
