81675944044eaf2cd9ded4a324c0d32506d7a51a
Michal Kacprzak <michal.kacprzak999@gmail.com>
2021-02-14 13:36:18 +0100

    create serializer for incoming data

--- music_controller/api/serializers.py
+++ music_controller/api/serializers.py
@@ -1,6 +1,14 @@
 from rest_framework import serializers
 from .models import Room
+
+
 class RoomSerializer(serializers.ModelSerializer):
     class Meta:
         model = Room
-        fields = ('id', 'code', 'host', 'quest_can_pose', 'votes_to_skip', 'created_at')
+        fields = ('id', 'code', 'host', 'guest_can_pause', 'votes_to_skip', 'created_at')
+
+
+class CreateRoomSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Room
+        fields = ('guest_can_pause', 'votes_to_skip')
