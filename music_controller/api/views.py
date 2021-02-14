63a92ac9560071c5259e63032dc5231a7c27bc9a
Michal Kacprzak <michal.kacprzak999@gmail.com>
2021-02-14 18:21:35 +0100

    simple form for adding new room with basic layout

--- music_controller/api/views.py
+++ music_controller/api/views.py
@@ -1,11 +1,12 @@
-from django.shortcuts import render
 from rest_framework import generics, status
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
+
 from .serializers import RoomSerializer, CreateRoomSerializer
 from .models import Room
 # Create your views here.
+
 
 class RoomView(generics.ListAPIView):
     queryset = Room.objects.all()
--- music_controller/frontend/src/pages/CreateRoomPage/CreateRoomPage.js
+++ music_controller/frontend/src/pages/CreateRoomPage/CreateRoomPage.js
@@ -1,7 +1,128 @@
 import React from 'react'
+import { Link } from "react-router-dom"
+import {
+    Button,
+    Grid,
+    Typography,
+    TextField,
+    FormHelperText,
+    FormControl,
+    Radio,
+    FormControlLabel,
+    RadioGroup
+} from '@material-ui/core'
+
+import {useForm, Controller} from "react-hook-form";
+
 
 const CreateRoomPage = () => {
-    return <p>This is CreateRoomPage</p>
+    const defaultVotes = 1
+    const { handleSubmit, control, reset} = useForm();
+
+    const onSubmit = async data => {
+
+      const response = await fetch('/api/create-room',{
+          method: 'POST',
+          credentials: 'omit',
+          headers: {'Content-Type': 'application/json'},
+          body: JSON.stringify(data)
+      });
+
+      const res = await response.json()
+      if(response.status === 201 || response.status === 200){
+          console.log(res)
+          reset()
+      } else {
+
+            alert(res)
+      }
+    };
+
+    return (
+        <Grid container spacing={1} alignItems={"flex-start"} alignContent={"flex-start"}>
+            <Grid item xs={12} align={"center"}>
+                <Typography component={"h4"} variant={"h4"}>
+                    Create a room
+                </Typography>
+            </Grid>
+
+            <Grid xs={12} align={"center"}>
+                <form onSubmit={handleSubmit(onSubmit)}>
+
+                    <Grid item xs={12} align={"center"}>
+                        <FormControl component={"fieldset"}>
+                            <FormHelperText>
+                                <div align={'center'}>
+                                    Guest Control of Playback State
+                                </div>
+                            </FormHelperText>
+                            <Controller
+                                name={"guest_can_pause"}
+                                as={
+                                    <RadioGroup row defaultValue={'true'}>
+                                        <FormControlLabel
+                                            control={<Radio color="primary"/>}
+                                            label={"Play/Pause"}
+                                            value={'true'}
+                                            labelPlacement={"bottom"}>
+
+                                        </FormControlLabel>
+                                        <FormControlLabel
+                                            control={<Radio color="secondary"/>}
+                                            label={"No control"}
+                                            value={'false'}
+                                            labelPlacement={"bottom"}>
+                                        </FormControlLabel>
+                                    </RadioGroup>
+                                }
+                                defaultValue={'true'}
+                                control={control}
+                            />
+                        </FormControl>
+                    </Grid>
+
+                    <Grid item xs={12} align="center">
+                        <FormControl>
+                            <Controller
+                                name={"votes_to_skip"}
+                                as={
+                                    <TextField
+                                        id={"requiredVotes"}
+                                        type={"number"}
+                                        inputProps={{
+                                            min: 1,
+                                            style: { textAlign: "center"}
+                                        }}
+                                    />
+                                }
+                                defaultValue={defaultVotes}
+                                control={control}
+                                rules={{
+                                    required: 'Required'
+                                }}
+                            />
+                            <FormHelperText>
+                                <div>Votes required to skip song</div>
+                            </FormHelperText>
+
+                        </FormControl>
+                    </Grid>
+
+                    <Grid item xs={12} align={"center"}>
+                        <Button color={"primary"} variant={"contained"} type={"submit"}>
+                            Create Room
+                        </Button>
+                    </Grid>
+                </form>
+            </Grid>
+
+            <Grid item xs={12} align={"center"}>
+                <Button color={"secondary"} variant={"contained"} to={"/"} component={Link}>
+                    Back
+                </Button>
+            </Grid>
+        </Grid>
+    );
 }
 
 export default CreateRoomPage
