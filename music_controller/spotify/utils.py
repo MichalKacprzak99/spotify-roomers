9df3da5b30cd564842a5d8227f4a9e10592946f9
Michal Kacprzak <michal.kacprzak999@gmail.com>
2021-02-15 19:32:05 +0100

    add endpoint for getting current playing song on spotify

--- music_controller/spotify/utils.py
+++ music_controller/spotify/utils.py
@@ -2,7 +2,7 @@ from .models import SpotifyToken
 from django.utils import timezone
 from datetime import timedelta
 from .credentials import Credentials
-from requests import post
+from requests import post, put, get
 
 
 BASE_URL = "https://api.spotify.com/v1/me/"
@@ -62,3 +62,20 @@ def refresh_spotify_token(session_id):
 
     update_or_create_user_tokens(
         session_id, access_token, token_type, expires_in, refresh_token)
+
+
+def execute_spotify_api_request(session_id, endpoint, post_=False, put_=False):
+    tokens = get_user_tokens(session_id)
+    headers = {'Content-Type': 'application/json',
+               'Authorization': "Bearer " + tokens.access_token}
+
+    if post_:
+        post(BASE_URL + endpoint, headers=headers)
+    if put_:
+        put(BASE_URL + endpoint, headers=headers)
+
+    response = get(BASE_URL + endpoint, {}, headers=headers)
+    try:
+        return response.json()
+    except ValueError:
+        return {'Error': 'Issue with request'}
--- music_controller/spotify/views.py
+++ music_controller/spotify/views.py
@@ -4,7 +4,9 @@ from rest_framework.views import APIView
 from requests import Request, post
 from rest_framework import status
 from rest_framework.response import Response
-from .utils import update_or_create_user_tokens, is_spotify_authenticated
+
+from .utils import update_or_create_user_tokens, is_spotify_authenticated, execute_spotify_api_request
+from api.models import Room
 
 
 class AuthURL(APIView):
@@ -54,6 +56,48 @@ class IsAuthenticated(APIView):
             self.request.session.session_key)
         return Response({'status': is_authenticated}, status=status.HTTP_200_OK)
 
+
+class CurrentSong(APIView):
+    def get(self, request, format=None):
+        room_code = self.request.session.get('room_code')
+        queryset = Room.objects.filter(code=room_code)
+        if queryset.exists():
+            room = queryset[0]
+        else:
+            return Response({}, status=status.HTTP_400_BAD_REQUEST)
+
+        host = room.host
+        endpoint = "player/currently-playing"
+        response = execute_spotify_api_request(host, endpoint)
+        if 'error' in response or 'item' not in response:
+            return Response({}, status=status.HTTP_204_NO_CONTENT)
+
+        item = response.get('item')
+        duration = item.get('duration_ms')
+        progress = response.get('progress_ms')
+        album_cover = item.get('album').get('images')[0].get('url')
+        is_playing = response.get('is_playing')
+        song_id = item.get('id')
+
+        artists_string = ""
+
+        for i, artist in enumerate(item.get('artists')):
+            if i > 0:
+                artists_string += ", "
+            artists_string += artist.get('name')
+
+        song = {
+            'title': item.get('name'),
+            'artists': artists_string,
+            'duration': duration,
+            'time': progress,
+            'image_url': album_cover,
+            'is_playing': is_playing,
+            'votes': 0,
+            'id': song_id,
+        }
+
+        return Response(song, status=status.HTTP_200_OK)
 
 
 
