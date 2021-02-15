49bc9ae6dc3aab8a5ff3e75e6da4e79487239eeb
Michal Kacprzak <michal.kacprzak999@gmail.com>
2021-02-15 09:56:54 +0100

    create basic layout of RoomPage

--- music_controller/frontend/src/pages/RoomPage/RoomPage.js
+++ music_controller/frontend/src/pages/RoomPage/RoomPage.js
@@ -1,31 +1,73 @@
 import React, {useState, useEffect} from 'react'
-import { useParams} from 'react-router-dom';
-const RoomPage = () => {
+import {Link, useParams, useHistory} from 'react-router-dom';
+import {Button, Grid, Typography} from "@material-ui/core";
+const RoomPage = (props) => {
     const [roomInfo, setRoomInfo] = useState(null)
 
     const {roomCode} = useParams();
-
-    const getRoomInfo = async() => {
-        const response = await fetch(`/api/get-room?code=${roomCode}`)
-
-        const res = await response.json()
-        if(response.status===200){
-            console.log(res)
-            setRoomInfo(res)
-        } else {
-            alert(res['Bad Request'])
-        }
-    }
+    let history = useHistory();
 
     useEffect(() => {
-        if(roomInfo == null){
-            getRoomInfo()
-        }
+        fetch(`/api/get-room?code=${roomCode}`)
+            .then(response => {
+                if(!response.ok){
+                    props.leaveRoomCallback()
+                    history.push('/')
+                }
+                return response.json()
+            })
+            .then(data => setRoomInfo(data))
     }, []);
 
+    const leaveRoom = () => {
+        fetch('/api/leave-room', {
+            method: "POST",
+            headers: {'Content-Type': 'application/json'},
+        }).then(_response =>{
+            props.leaveRoomCallback()
+            history.push('/')
+        })
+
+    }
+
+    const renderRoomPage = () => {
+        const {guest_can_pose: guestCanPose, votes_to_skip: votesToSkip, is_host: isHost } = roomInfo
+        return (
+            <Grid container spacing={1} alignItems={"center"} direction={"column"}>
+                <Grid item xs={12} >
+                    <Typography variant={"h4"} component={"h4"}>
+                        Code: {roomCode}
+                    </Typography>
+                </Grid>
+                <Grid item xs={12} >
+                    <Typography variant={"h6"} component={"h4"}>
+                        Code: {guestCanPose}
+                    </Typography>
+                </Grid>
+                <Grid item xs={12} >
+                    <Typography variant={"h6"} component={"h4"}>
+                        Code: {votesToSkip}
+                    </Typography>
+                </Grid>
+                <Grid item xs={12} >
+                    <Typography variant={"h6"} component={"h4"}>
+                        Code: {isHost.toString()}
+                    </Typography>
+                </Grid>
+                <Grid item xs={12}>
+                    <Button color={"secondary"} variant={"contained"} onClick={leaveRoom} >
+                        Leave Room
+                    </Button>
+                </Grid>
+            </Grid>
+        )
+    }
+
     return (
-        <p>This is room page, {roomCode}</p>
-    )
+        <>
+            {roomInfo ? renderRoomPage(): null}
+        </>
+    );
 }
 
 export default RoomPage
