f444ad3108fdf2cbec895518a12d08a38b803f37
Michal Kacprzak <michal.kacprzak999@gmail.com>
2021-02-15 08:17:42 +0100

    create basic layout of HomePage

--- music_controller/frontend/src/pages/HomePage/HomePage.js
+++ music_controller/frontend/src/pages/HomePage/HomePage.js
@@ -1,7 +1,27 @@
 import React from 'react'
+import {Button, ButtonGroup, Grid, Typography} from "@material-ui/core";
+import {Link} from "react-router-dom";
 
 const HomePage = () => {
-    return <p>This is home page</p>
+    return (
+     <Grid container spacing={3}>
+        <Grid item xs={12} align={"center"}>
+          <Typography variant="h3" compact="h3">
+            House Party
+          </Typography>
+        </Grid>
+        <Grid item xs={12} align={"center"}>
+          <ButtonGroup disableElevation variant="contained" color="primary">
+            <Button color="primary" to="/join" component={Link}>
+              Join a Room
+            </Button>
+            <Button color="secondary" to="/create" component={Link}>
+              Create a Room
+            </Button>
+          </ButtonGroup>
+        </Grid>
+      </Grid>
+    );
 }
 
 export default HomePage
