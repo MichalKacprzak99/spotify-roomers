bc90f60f68819e6c20af23f961cae857d3b9ec8d
Michal Kacprzak <michal.kacprzak999@gmail.com>
2021-02-15 22:26:46 +0100

    hide secret key

--- music_controller/music_controller/settings.py
+++ music_controller/music_controller/settings.py
@@ -9,9 +9,13 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
+import os
 
 from pathlib import Path
+from dotenv import load_dotenv, find_dotenv
 
+
+load_dotenv(find_dotenv())
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,10 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'x4dh_brq)ekzjw%b7b(he9%%b$%cb9i&zo9wxnq&a&huyreh-z'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG')
 
 ALLOWED_HOSTS = []
 
