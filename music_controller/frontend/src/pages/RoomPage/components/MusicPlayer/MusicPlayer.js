85c73e9d763506b727493131295e86aa3c126c9b
Michal Kacprzak <michal.kacprzak999@gmail.com>
2021-02-16 16:48:15 +0100

    code refactoring | add info when premium is required

--- music_controller/frontend/src/pages/RoomPage/components/MusicPlayer/MusicPlayer.js
+++ music_controller/frontend/src/pages/RoomPage/components/MusicPlayer/MusicPlayer.js
@@ -1,21 +1,38 @@
-import React from "react";
+import React, {useState} from "react";
 import {
-  Grid,
-  Typography,
-  Card,
-  IconButton,
-  LinearProgress,
+    Grid,
+    Typography,
+    Card,
+    IconButton,
+    LinearProgress, Collapse,
 } from "@material-ui/core";
 import PlayArrowIcon from "@material-ui/icons/PlayArrow";
 import PauseIcon from "@material-ui/icons/Pause";
 import SkipNextIcon from "@material-ui/icons/SkipNext";
+import Alert from "@material-ui/lab/Alert";
+import defaultAlbumCover from './images/default-album-art.png'
 
-const MusicPlayer = ({song}) => {
+
+const MusicPlayer = ({song, votesRequired}) => {
+    const [playerMessage, setPlayerMessage] = useState("")
+    const {
+        image_url: imageUrl = defaultAlbumCover,
+        title = "No song",
+        artists,
+        is_playing: isPlaying,
+        time, duration,
+        votes=0,
+    } = song
+    const songProgress = (time/duration) * 100
 
     const pauseSong = () => {
         fetch('/spotify/pause', {
             method: "PUT",
             headers: {'Content-Type': 'application/json'},
+        }).then((response) => {
+            if (response.status === 405){
+                setPlayerMessage("Premium required")
+            }
         })
     }
 
@@ -23,6 +40,10 @@ const MusicPlayer = ({song}) => {
         fetch('/spotify/play', {
             method: "PUT",
             headers: {'Content-Type': 'application/json'},
+        }).then((response) => {
+            if (response.status === 405){
+                setPlayerMessage("Premium required")
+            }
         })
     }
 
@@ -30,25 +51,39 @@ const MusicPlayer = ({song}) => {
         fetch('/spotify/skip', {
             method: "POST",
             headers: {'Content-Type': 'application/json'},
+        }).then((response) => {
+            if (response.status === 405){
+                setPlayerMessage("Premium required")
+            }
         })
     }
 
+    const renderPlayerMessage = () => {
+        return (
+            <Collapse in={playerMessage !== ""}>
+                <Alert severity={"error"} onClose={()=>{setPlayerMessage("")}}>{playerMessage}</Alert>
+            </Collapse>
+        )
+    }
 
-    const {
-        image_url: imageUrl,
-        title,
-        artists,
-        is_playing: isPlaying,
-        time, duration,
-        votes,
-        votes_required: votesRequired,
-    } = song
-    const songProgress = (time/duration) * 100
+    const renderSongControl = () => {
+        return (
+            <div>
+                <IconButton onClick={() => isPlaying ? pauseSong() : playSong()}>
+                    {isPlaying ? <PauseIcon /> : <PlayArrowIcon/>}
+                </IconButton>
+                <IconButton onClick={() => skipSong()}>
+                    <SkipNextIcon/>{votes} /{" "} {votesRequired}
+                </IconButton>
+            </div>
+        )
+    }
+
     return (
         <Card>
             <Grid container  alignItems={"center"} direction={"row"}>
                 <Grid item xs={4}>
-                    <img src={imageUrl} height={"100%"} width={"100%"}/>
+                    <img src={imageUrl} height={"100%"} width={"100%"} alt={"Album Cover"}/>
                 </Grid>
                 <Grid item align="center" xs={8}>
                     <Typography component={"h5"} variant={"h5"}>
@@ -57,14 +92,8 @@ const MusicPlayer = ({song}) => {
                     <Typography component={"h5"} variant={"subtitle1"} color={"textSecondary"}>
                         {artists}
                     </Typography>
-                    <div>
-                        <IconButton onClick={() => isPlaying ? pauseSong() : playSong()}>
-                            {isPlaying ? <PauseIcon /> : <PlayArrowIcon/>}
-                        </IconButton>
-                        <IconButton onClick={() => skipSong()}>
-                            <SkipNextIcon/>{votes} /{" "} {votesRequired}
-                        </IconButton>
-                    </div>
+                    {Object.keys(song).length !== 0 ? renderSongControl() : null}
+                    {renderPlayerMessage()}
                 </Grid>
             </Grid>
             <LinearProgress variant={"determinate"} value={songProgress}/>
