cf1394ec8e204ea303ed6601553968b88240ec52
Michal Kacprzak <michal.kacprzak999@gmail.com>
2021-02-16 17:34:23 +0100

    renaming

--- spotify_roomers/spotify_roomers/urls.py
+++ spotify_roomers/spotify_roomers/urls.py
@@ -1,4 +1,4 @@
-"""music_controller URL Configuration
+"""spotify_roomers URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.1/topics/http/urls/
